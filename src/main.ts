081fe0b0a6c4d6a499b5241f2946a82102f8c850
Son Daehyeon <sondaehyeon01@gmail.com>
2024-07-15 19:27:53 +0900

    chore: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+
+  SwaggerModule.setup(
+    'swagger',
+    app,
+    SwaggerModule.createDocument(
+      app,
+      new DocumentBuilder()
+        .setTitle('Wink Official')
+        .setDescription('윙크 공식 홈페이지 API 명세서')
+        .setVersion('1.0.0')
+        .build(),
+    ),
+    {
+      swaggerOptions: { defaultModelsExpandDepth: -1 },
+    },
+  );
 
   await app.listen(3000);
 }
