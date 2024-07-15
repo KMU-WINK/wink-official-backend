558c004c40210f6e774fd627071da2a6dc1d5b93
Son Daehyeon <sondaehyeon01@gmail.com>
2024-07-16 02:10:25 +0900

    chore: add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { RequestLoggingMiddleware } from './utils/logger/RequestLoggingMiddleware';
 import configuration from './utils/config/configuration';
+import { MongooseConfigService } from './utils/mongo/MongooseConfigService';
 
 @Module({
-  imports: [ConfigModule.forRoot({ load: [configuration], isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
+
+    MongooseModule.forRootAsync({
+      useClass: MongooseConfigService,
+    }),
+  ],
   controllers: [AppController],
   providers: [RequestLoggingMiddleware, AppService],
 })
