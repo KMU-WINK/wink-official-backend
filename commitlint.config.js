2bea8181839513c2de933b86f97978c60b7fc701
Son Daehyeon <sondaehyeon01@gmail.com>
2024-07-17 00:50:51 +0900

    fix: 커밋 메시지 한글도 가능하도록 수정

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,24 +1,8 @@
+const type = ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'refactor', 'revert', 'style', 'test'];
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'build',
-        'chore',
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'style',
-        'test',
-        'sample',
-      ],
-    ],
+    'type-enum': [2, 'always', type],
   },
 };
