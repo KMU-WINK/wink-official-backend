a8cae2ba8841bb537ece5a2e090e1210b58df741
Son Daehyeon <sondaehyeon01@gmail.com>
2024-07-17 14:54:15 +0900

    chore: NodeMailer 모듈 추가

---
+++ src/utils/mail/NodeMail.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as nodemailer from 'nodemailer';
+import { Transporter } from 'nodemailer';
+
+@Injectable()
+export class NodeMail {
+  private readonly transporter: Transporter;
+
+  constructor(private readonly configService: ConfigService) {
+    this.transporter = nodemailer.createTransport({
+      host: configService.getOrThrow<string>('smtp.host'),
+      port: configService.getOrThrow<number>('smtp.port'),
+      secure: configService.getOrThrow<boolean>('smtp.secure'),
+      auth: {
+        user: configService.getOrThrow<string>('smtp.username'),
+        pass: configService.getOrThrow<string>('smtp.password'),
+      },
+    });
+  }
+
+  async sendMail(to: string, subject: string, html: string): Promise<void> {
+    await this.transporter.sendMail({
+      from: this.configService.getOrThrow<string>('smtp.username'),
+      to,
+      subject,
+      html,
+    });
+  }
+}
