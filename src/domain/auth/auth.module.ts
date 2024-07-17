3cb7d0e66d9dfe7e92ef7372a54983da1a9ea4f8
Son Daehyeon <sondaehyeon01@gmail.com>
2024-07-17 15:12:23 +0900

    feat: 인증코드 발급/검증 로직

--- src/domain/auth/auth.module.ts
+++ src/domain/auth/auth.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { MailModule } from '../../utils/mail/mail.module';
+import { RedisModule } from '../../utils/redis/redis.module';
 
 @Module({
+  imports: [MailModule, RedisModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/domain/auth/auth.service.ts
+++ src/domain/auth/auth.service.ts
@@ -1,4 +1,53 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
 
+import { v4 as uuid } from 'uuid';
+
+import { NodeMail } from '../../utils/mail/NodeMail';
+import { RedisRepository } from '../../utils/redis/RedisRepository';
+
+import { InvalidVerifyCodeException } from './exception/InvalidVerifyCodeException';
+
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly nodeMail: NodeMail,
+    private readonly redis: RedisRepository,
+  ) {}
+
+  async login(email: string, password: string): Promise<string> {
+    return '';
+  }
+
+  async register(
+    name: string,
+    studentId: number,
+    password: string,
+    verifyToken: string,
+  ): Promise<void> {}
+
+  async sendCode(email: string): Promise<void> {
+    const code = Math.floor(Math.random() * 1_000_000)
+      .toString()
+      .padStart(6, '0');
+
+    await this.redis.setex(email, code, 60 * 10);
+
+    await this.nodeMail.sendMail(email, '[WINK] 회원가입 인증코드', `인증코드: ${code}`);
+  }
+
+  async verifyCode(email: string, code: string): Promise<string> {
+    const storedCode = await this.redis.get(email);
+
+    if (storedCode !== code) {
+      throw new InvalidVerifyCodeException();
+    }
+
+    await this.redis.delete(email);
+
+    const verifyToken = uuid();
+    await this.redis.setex(verifyToken, email, 60 * 60);
+
+    return verifyToken;
+  }
+}
