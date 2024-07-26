7ed9af8eb7d417c20da364fc59219760387b2c96
Son Daehyeon <sondaehyeon01@gmail.com>
2024-07-26 20:15:48 +0900

    refactor: 테스트시 사용되는 mocking 전용 유틸 함수 추가

---
+++ src/domain/auth/spec/mock/auth.mock.ts
@@ -0,0 +1,104 @@
+import { Test } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
+
+import { Member } from '../../../member/member.schema';
+
+import { AuthController } from '../../auth.controller';
+import { AuthService } from '../../auth.service';
+import { MemberRepository } from '../../../member/member.repository';
+import { RedisRepository } from '../../../../utils/redis/RedisRepository';
+import { NodeMail } from '../../../../utils/mail/NodeMail';
+
+export const mockAuth = async () => {
+  const memoryMemberRepository: Member[] = [];
+  const memoryRedisRepository: Record<string, string> = {};
+
+  const module = await Test.createTestingModule({
+    controllers: [AuthController],
+    providers: [
+      AuthService,
+      { provide: ConfigService, useValue: mockConfigService() },
+      { provide: MemberRepository, useValue: mockMemberRepository(memoryMemberRepository) },
+      { provide: RedisRepository, useValue: mockRedisRepository(memoryRedisRepository) },
+      { provide: NodeMail, useValue: mockNodeMail() },
+    ],
+  }).compile();
+
+  return {
+    module,
+    memoryMemberRepository,
+    memoryRedisRepository,
+  };
+};
+
+const mockConfigService = () => ({
+  getOrThrow: jest.fn().mockImplementation((key: string) => {
+    switch (key) {
+      case 'jwt.secret':
+        return 'jwt_secret_for_test';
+      case 'jwt.expiresIn':
+        return '1h';
+      default:
+        throw new Error(`Unknown key: ${key}`);
+    }
+  }),
+});
+
+const mockMemberRepository = (memory: Member[]) => ({
+  save: jest.fn().mockImplementation(async (member: Partial<Member>) => {
+    member['_id'] = memory.length.toString();
+    memory.push(member as Member);
+    return member as Member;
+  }),
+
+  findById: jest.fn().mockImplementation(async (id: string) => {
+    return memory.find((member) => member['_id'] === id);
+  }),
+
+  findByEmailWithPassword: jest.fn().mockImplementation(async (email: string) => {
+    return memory.find((member) => member.email === email);
+  }),
+
+  existsById: jest.fn().mockImplementation(async (id: string) => {
+    return memory.some((member) => member['_id'] === id);
+  }),
+
+  existsByEmail: jest.fn().mockImplementation(async (email: string) => {
+    return memory.some((member) => member.email === email);
+  }),
+
+  existsByStudentId: jest.fn().mockImplementation(async (studentId: number) => {
+    return memory.some((member) => member.studentId === studentId);
+  }),
+});
+
+const mockRedisRepository = (memory: Record<string, string>) => ({
+  get: jest.fn().mockImplementation(async (key: string) => {
+    return memory[key];
+  }),
+
+  set: jest.fn().mockImplementation(async (key: string, value: string) => {
+    memory[key] = value;
+  }),
+
+  ttl: jest.fn().mockImplementation(async (key: string, value: string, seconds: number) => {
+    memory[key] = value;
+    setTimeout(() => {
+      if (key in memory) {
+        delete memory[key];
+      }
+    }, seconds * 1000);
+  }),
+
+  delete: jest.fn().mockImplementation(async (key: string) => {
+    return delete memory[key];
+  }),
+
+  exists: jest.fn().mockImplementation(async (key: string) => {
+    return key in memory;
+  }),
+});
+
+const mockNodeMail = () => ({
+  sendMail: jest.fn(),
+});
