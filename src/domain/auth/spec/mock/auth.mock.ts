789afd99fd10de4570f34f20ce0cf042cd43d83e
Son Daehyeon <sondaehyeon01@gmail.com>
2024-07-29 09:51:32 +0900

    fix(auth): test mocking 최적화

--- src/domain/auth/spec/mock/auth.mock.ts
+++ src/domain/auth/spec/mock/auth.mock.ts
@@ -32,7 +32,7 @@ export const mockAuth = async () => {
 };
 
 const mockConfigService = () => ({
-  getOrThrow: jest.fn().mockImplementation((key: string) => {
+  getOrThrow: jest.fn((key: string) => {
     switch (key) {
       case 'jwt.secret':
         return 'jwt_secret_for_test';
@@ -45,55 +45,55 @@ const mockConfigService = () => ({
 });
 
 const mockMemberRepository = (memory: Member[]) => ({
-  save: jest.fn().mockImplementation(async (member: Partial<Member>) => {
+  save: jest.fn(async (member: Partial<Member>) => {
     member['_id'] = memory.length.toString();
     memory.push(member as Member);
     return member as Member;
   }),
 
-  findById: jest.fn().mockImplementation(async (id: string) => {
+  findById: jest.fn(async (id: string) => {
     return memory.find((member) => member['_id'] === id);
   }),
 
-  findByEmailWithPassword: jest.fn().mockImplementation(async (email: string) => {
+  findByEmailWithPassword: jest.fn(async (email: string) => {
     return memory.find((member) => member.email === email);
   }),
 
-  existsById: jest.fn().mockImplementation(async (id: string) => {
+  existsById: jest.fn(async (id: string) => {
     return memory.some((member) => member['_id'] === id);
   }),
 
-  existsByEmail: jest.fn().mockImplementation(async (email: string) => {
+  existsByEmail: jest.fn(async (email: string) => {
     return memory.some((member) => member.email === email);
   }),
 
-  existsByStudentId: jest.fn().mockImplementation(async (studentId: number) => {
+  existsByStudentId: jest.fn(async (studentId: number) => {
     return memory.some((member) => member.studentId === studentId);
   }),
 });
 
 const mockRedisRepository = (memory: Record<string, string>) => ({
-  get: jest.fn().mockImplementation(async (key: string) => {
+  get: jest.fn(async (key: string) => {
     return memory[key];
   }),
 
-  set: jest.fn().mockImplementation(async (key: string, value: string) => {
+  set: jest.fn(async (key: string, value: string) => {
     memory[key] = value;
   }),
 
-  ttl: jest.fn().mockImplementation(async (key: string, value: string) => {
+  ttl: jest.fn(async (key: string, value: string) => {
     memory[key] = value;
   }),
 
-  delete: jest.fn().mockImplementation(async (key: string) => {
+  delete: jest.fn(async (key: string) => {
     return delete memory[key];
   }),
 
-  exists: jest.fn().mockImplementation(async (key: string) => {
+  exists: jest.fn(async (key: string) => {
     return key in memory;
   }),
 });
 
 const mockNodeMail = () => ({
-  sendMail: jest.fn(),
+  sendMail: jest.fn(async () => {}),
 });
