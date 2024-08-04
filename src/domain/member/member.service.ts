77cd8eaa4cfc7f2e679541009f9414a1abfa7dd9
Son Daehyeon <sondaehyeon01@gmail.com>
2024-08-04 11:03:38 +0900

    feat(member): 아바타 변경 시 기존 아바타 삭제

--- src/domain/member/member.service.ts
+++ src/domain/member/member.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { DeleteObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
 import * as bcrypt from 'bcrypt';
 
@@ -8,10 +11,19 @@ import { Member } from './member.schema';
 import { MemberRepository } from './member.repository';
 
 import { WrongPasswordException } from '../auth/exception';
+import { S3Provider } from '../../utils';
 
 @Injectable()
 export class MemberService {
-  constructor(private readonly memberRepository: MemberRepository) {}
+  private readonly s3Client: S3Client;
+
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly memberRepository: MemberRepository,
+    s3Provider: S3Provider,
+  ) {
+    this.s3Client = s3Provider.getS3Client();
+  }
 
   async getMembers(): Promise<EachGetMembersResponseDto[]> {
     const members = await this.memberRepository.findAll();
@@ -61,9 +73,14 @@ export class MemberService {
 
   async updateMyAvatar(member: Member, file: Express.MulterS3.File): Promise<string> {
     const id = member['_id'];
-    const avatar = file.location;
+    const original = member.avatar;
 
+    const avatar = file.location;
     await this.memberRepository.updateAvatar(id, avatar);
+
+    const bucket = this.configService.getOrThrow<string>('s3.bucket');
+    const key = original.split('/').slice(-1)[0];
+    await this.s3Client.send(new DeleteObjectCommand({ Bucket: bucket, Key: key }));
 
     return avatar;
   }
