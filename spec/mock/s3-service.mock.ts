14aec68147c8f51db71f45c3fb4491a017c74d58
Son Daehyeon <sondaehyeon01@gmail.com>
2024-08-05 18:45:30 +0900

    test(s3): mock object의 return값 지정

--- spec/mock/s3-service.mock.ts
+++ spec/mock/s3-service.mock.ts
@@ -1,5 +1,7 @@
 export const mockS3Service = () => ({
-  upload: jest.fn(),
+  upload: jest.fn(
+    async (file: Express.Multer.File) => `https://s3.amazonaws.com/${file.originalname}`,
+  ),
   delete: jest.fn(),
   deleteFromUrl: jest.fn(),
 });
