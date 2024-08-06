316c4f91cc025c3006bf8a9fc41c98471c99f684
Son Daehyeon <sondaehyeon01@gmail.com>
2024-08-07 00:25:17 +0900

    chore: config 파일 정리 (#57)

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -3,5 +3,9 @@ const type = ['build', 'chore', 'ci', 'docs', 'feat', 'fix', 'refactor', 'revert
 export default {
   rules: {
     'type-enum': [2, 'always', type],
+    'type-empty': [2, 'never'],
+    'type-case': [2, 'always', ['lower-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
   },
 };
--- jest.config.mjs
+++ jest.config.mjs
@@ -8,27 +8,27 @@ export default {
   testEnvironment: 'node',
   moduleNameMapper: {
     // Comon
-    "^@wink/app$": "<rootDir>/../src/common/app",
-    "^@wink/config$": "<rootDir>/../src/common/config",
-    "^@wink/filter$": "<rootDir>/../src/common/filter",
-    "^@wink/interceptor$": "<rootDir>/../src/common/interceptor",
-    "^@wink/mongo$": "<rootDir>/../src/common/mongo",
-    "^@wink/redis$": "<rootDir>/../src/common/redis",
-    "^@wink/s3$": "<rootDir>/../src/common/s3",
+    '^@wink/app$': '<rootDir>/../src/common/app',
+    '^@wink/config$': '<rootDir>/../src/common/config',
+    '^@wink/filter$': '<rootDir>/../src/common/filter',
+    '^@wink/interceptor$': '<rootDir>/../src/common/interceptor',
+    '^@wink/mongo$': '<rootDir>/../src/common/mongo',
+    '^@wink/redis$': '<rootDir>/../src/common/redis',
+    '^@wink/s3$': '<rootDir>/../src/common/s3',
 
     // Utils
-    "^@wink/event$": "<rootDir>/../src/common/util/event",
-    "^@wink/logger$": "<rootDir>/../src/common/util/logger",
-    "^@wink/mail$": "<rootDir>/../src/common/util/mail",
-    "^@wink/swagger$": "<rootDir>/../src/common/util/swagger",
-    "^@wink/validation$": "<rootDir>/../src/common/util/validation",
+    '^@wink/event$': '<rootDir>/../src/common/util/event',
+    '^@wink/logger$': '<rootDir>/../src/common/util/logger',
+    '^@wink/mail$': '<rootDir>/../src/common/util/mail',
+    '^@wink/swagger$': '<rootDir>/../src/common/util/swagger',
+    '^@wink/validation$': '<rootDir>/../src/common/util/validation',
 
     // Domain
-    "^@wink/auth/(.*)$": "<rootDir>/../src/domain/auth/$1",
-    "^@wink/member/(.*)$": "<rootDir>/../src/domain/member/$1",
-    "^@wink/activity/(.*)$": "<rootDir>/../src/domain/activity/$1",
+    '^@wink/auth/(.*)$': '<rootDir>/../src/domain/auth/$1',
+    '^@wink/member/(.*)$': '<rootDir>/../src/domain/member/$1',
+    '^@wink/activity/(.*)$': '<rootDir>/../src/domain/activity/$1',
 
     // Test
-    "^@wink/test-mock$": "<rootDir>/mock"
-  }
+    '^@wink/test-mock$': '<rootDir>/mock',
+  },
 };
