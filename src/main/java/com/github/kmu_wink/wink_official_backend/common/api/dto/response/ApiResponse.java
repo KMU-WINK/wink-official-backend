3a1784a13958f3b22d517f6aa971d2de975a68d2
Son Daehyeon <sondaehyeon01@gmail.com>
2024-09-20 12:20:34 +0900

    feat: 공통 응답 포맷 구현

---
+++ src/main/java/com/github/kmu_wink/wink_official_backend/common/api/dto/response/ApiResponse.java
@@ -0,0 +1,36 @@
+package com.github.kmu_wink.wink_official_backend.common.api.dto.response;
+
+import com.github.kmu_wink.wink_official_backend.common.api.exception.ApiException;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class ApiResponse<T> {
+
+    private final int statusCode;
+    private final String error;
+    private final T content;
+
+    public static ApiResponse<Void> ok() {
+
+        return new ApiResponse<>(200, null, null);
+    }
+
+    public static <T> ApiResponse<T> ok(T content) {
+
+        return new ApiResponse<>(200, null, content);
+    }
+
+    public static <T> ApiResponse<T> error(ApiException e) {
+
+        return error(e.getHttpStatus(), e.getMessage());
+    }
+
+    public static <T> ApiResponse<T> error(HttpStatus status, String error) {
+
+        return new ApiResponse<>(status.value(), error, null);
+    }
+}
