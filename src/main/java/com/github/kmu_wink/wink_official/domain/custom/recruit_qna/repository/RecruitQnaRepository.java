9a435c27242a2836918d249975a3036842648a54
Son Daehyeon <sondaehyeon01@gmail.com>
2024-11-10 03:30:08 +0900

    rollback: Custom 페이지 삭제

--- src/main/java/com/github/kmu_wink/wink_official/domain/custom/recruit_qna/repository/RecruitQnaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.github.kmu_wink.wink_official.domain.custom.recruit_qna.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.github.kmu_wink.wink_official.domain.custom.recruit_qna.schema.RecruitQna;
-
-public interface RecruitQnaRepository extends MongoRepository<RecruitQna, String> {
-}
