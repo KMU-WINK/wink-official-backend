c255aff3066c77fced359ec9882124ac774d7a1f
Son Daehyeon <sondaehyeon01@gmail.com>
2025-03-09 06:26:31 +0900

    chore: 프로젝트 라이센스 추가

--- src/main/java/com/github/son_daehyeon/Application.java
+++ src/main/java/com/github/son_daehyeon/Application.java
@@ -4,13 +4,8 @@ import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import com.github.son_daehyeon.common.property.JwtProperty;
-import com.github.son_daehyeon.common.property.RedisProperty;
 
 @SpringBootApplication
-@EnableConfigurationProperties({JwtProperty.class, RedisProperty.class})
 public class Application {
 
     public static void main(String[] args) {
